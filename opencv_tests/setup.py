8d69f65ba82aa26b937ac555832b83e0dd2d82f1
Kenji Brameld <kenjibrameld@gmail.com>
2022-09-14 19:49:38 +0900

    Rebase from 'upstream'

--- opencv_tests/setup.py
+++ opencv_tests/setup.py
@@ -5,7 +5,7 @@ package_name = 'opencv_tests'
 
 setup(
     name=package_name,
-    version='3.0.3',
+    version='3.2.0',
     packages=find_packages(exclude=['launch']),
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -16,8 +16,8 @@ setup(
     zip_safe=True,
     author='Ethan Gao',
     author_email='ethan.gao@linux.intel.com',
-    maintainer='Ethan Gao',
-    maintainer_email='ethan.gao@linux.intel.com',
+    maintainer='Kenji Brameld',
+    maintainer_email='kenjibrameld@gmail.com',
     keywords=['ROS'],
     classifiers=[
         'Intended Audience :: Developers',
