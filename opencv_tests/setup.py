f7fda86ba8ec0ae76d84dc8d9a52a4430bd4662c
ijnek <kenjibrameld@gmail.com>
2024-04-13 14:48:11 +0000

    Rebase from 'upstream'

--- opencv_tests/setup.py
+++ opencv_tests/setup.py
@@ -5,7 +5,7 @@ package_name = 'opencv_tests'
 
 setup(
     name=package_name,
-    version='3.4.0',
+    version='3.5.0',
     packages=find_packages(exclude=['launch']),
     data_files=[
         ('share/ament_index/resource_index/packages',
